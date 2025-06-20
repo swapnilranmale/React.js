ff2a7fe112684b826c06dda1e810f995eb6b7f5a
swapnilranmale <ranmale.swapnil@thinkitive.com>
2025-06-20 15:37:02 +0530

    list

--- project_01/src/pages/About.jsx
+++ project_01/src/pages/About.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  const list = ["CSK", "MI", "RCB", "KKR", "SRH", "DC", "PBKS"];
+  const games = ["cricket", "abc"];
+  return (
+    <div>
+      <h1>List</h1>
+      {list.map((i, index) => {
+        return (
+          <h1 key={index}>
+            {String.fromCharCode(97 + index)}.{i}
+          </h1>
+        );
+      })}
+      <ul>
+        {games.map((item, index) => {
+          if (index === 1) {
+            console.log("Index 1 element:", item);
+          }
+          return (
+            <h1 key={index}>
+              {index + 1}. {item}
+            </h1>
+          );
+        })}
+      </ul>
+
+      {list
+        .filter((item) => item.charAt(0) === "K")
+        .map((team, index) => (
+          <h1 key={index}>{team}</h1>
+        ))}
+    </div>
+  );
 }
 
 export default About;
